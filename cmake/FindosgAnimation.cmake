f4474d1329851fea7767a76b25cc565720dcb41a
johannes_d <deselaers.johannes@gmail.com>
2013-10-31 10:58:49 +0100

    added Findosg*.cmake files from OSG Beginners Guide Examples

---
+++ cmake/FindosgAnimation.cmake
@@ -0,0 +1,88 @@
+# This is part of the Findosg* suite used to find OpenSceneGraph components.
+# Each component is separate and you must opt in to each module. You must 
+# also opt into OpenGL and OpenThreads (and Producer if needed) as these 
+# modules won't do it for you. This is to allow you control over your own 
+# system piece by piece in case you need to opt out of certain components
+# or change the Find behavior for a particular module (perhaps because the
+# default FindOpenGL.cmake module doesn't work with your system as an
+# example).
+# If you want to use a more convenient module that includes everything,
+# use the FindOpenSceneGraph.cmake instead of the Findosg*.cmake modules.
+# 
+# Locate osgAnimation
+# This module defines
+# OSGANIMATION_LIBRARY
+# OSGANIMATION_FOUND, if false, do not try to link to osgAnimation
+# OSGANIMATION_INCLUDE_DIR, where to find the headers
+#
+# $OSGDIR is an environment variable that would
+# correspond to the ./configure --prefix=$OSGDIR
+# used in building osg.
+#
+# Created by Eric Wing.
+
+# Header files are presumed to be included like
+# #include <osg/PositionAttitudeTransform>
+# #include <osgAnimation/Animation>
+
+# Try the user's environment request before anything else.
+FIND_PATH(OSGANIMATION_INCLUDE_DIR osgAnimation/Animation
+  HINTS
+  $ENV{OSGANIMATION_DIR}
+  $ENV{OSG_DIR}
+  $ENV{OSGDIR}
+  PATH_SUFFIXES include
+  PATHS
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local
+    /usr
+    /sw # Fink
+    /opt/local # DarwinPorts
+    /opt/csw # Blastwave
+    /opt
+    [HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session\ Manager\\Environment;OpenThreads_ROOT]
+    [HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session\ Manager\\Environment;OSG_ROOT]
+)
+
+FIND_LIBRARY(OSGANIMATION_LIBRARY 
+  NAMES osgAnimation
+  HINTS
+  $ENV{OSGANIMATION_DIR}
+  $ENV{OSG_DIR}
+  $ENV{OSGDIR}
+  PATH_SUFFIXES lib64 lib
+  PATHS
+    ~/Library/Frameworks
+    /Library/Frameworks
+  /usr/local
+  /usr
+  /sw
+  /opt/local
+  /opt/csw
+  /opt
+)
+
+FIND_LIBRARY(OSGANIMATION_LIBRARY_DEBUG 
+  NAMES osgAnimationd
+  HINTS
+  $ENV{OSGANIMATION_DIR}
+  $ENV{OSG_DIR}
+  $ENV{OSGDIR}
+  PATH_SUFFIXES lib64 lib
+  PATHS
+    ~/Library/Frameworks
+    /Library/Frameworks
+  /usr/local
+  /usr
+  /sw
+  /opt/local
+  /opt/csw
+  /opt
+)
+
+SET(OSGANIMATION_FOUND "NO")
+IF(OSGANIMATION_LIBRARY AND OSGANIMATION_INCLUDE_DIR)
+  SET(OSGANIMATION_FOUND "YES")
+ENDIF(OSGANIMATION_LIBRARY AND OSGANIMATION_INCLUDE_DIR)
+
