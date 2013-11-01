d5646545dfaeefc4fcf6311a886ff0e832748751
Philipp Otto <philipp@scm.io>
2013-11-01 18:37:41 +0100

    extract bullet calls to testBullet method

--- source/main.cpp
+++ source/main.cpp
@@ -8,92 +8,85 @@
 #include <btBulletDynamicsCommon.h>
 #include <iostream>
 
+
+void testBullet() {
+	btBroadphaseInterface* broadphase = new btDbvtBroadphase();
+
+	btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
+	btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
+
+	btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
+
+	btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
+
+	dynamicsWorld->setGravity(btVector3(0, -10, 0));
+
+
+	btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0, 1, 0), 1);
+
+	btCollisionShape* fallShape = new btSphereShape(1);
+
+
+	btDefaultMotionState* groundMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, -1, 0)));
+	btRigidBody::btRigidBodyConstructionInfo
+		groundRigidBodyCI(0, groundMotionState, groundShape, btVector3(0, 0, 0));
+	btRigidBody* groundRigidBody = new btRigidBody(groundRigidBodyCI);
+	dynamicsWorld->addRigidBody(groundRigidBody);
+
+
+	btDefaultMotionState* fallMotionState =
+		new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 50, 0)));
+	btScalar mass = 1;
+	btVector3 fallInertia(0, 0, 0);
+	fallShape->calculateLocalInertia(mass, fallInertia);
+	btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass, fallMotionState, fallShape, fallInertia);
+	btRigidBody* fallRigidBody = new btRigidBody(fallRigidBodyCI);
+	dynamicsWorld->addRigidBody(fallRigidBody);
+
+
+	for (int i = 0; i<300; i++) {
+		dynamicsWorld->stepSimulation(1 / 60.f, 10);
+
+		btTransform trans;
+		fallRigidBody->getMotionState()->getWorldTransform(trans);
+
+		std::cout << "sphere height: " << trans.getOrigin().getY() << std::endl;
+	}
+
+	dynamicsWorld->removeRigidBody(fallRigidBody);
+	delete fallRigidBody->getMotionState();
+	delete fallRigidBody;
+
+	dynamicsWorld->removeRigidBody(groundRigidBody);
+	delete groundRigidBody->getMotionState();
+	delete groundRigidBody;
+
+
+	delete fallShape;
+	delete groundShape;
+	delete dynamicsWorld;
+	delete solver;
+	delete collisionConfiguration;
+	delete dispatcher;
+	delete broadphase;
+
+}
+
 int main(int argc, char* argv[])
 {
 
-		btBroadphaseInterface* broadphase = new btDbvtBroadphase();
-
-		btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
-		btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
-
-		btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
-
-		btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
-
-		dynamicsWorld->setGravity(btVector3(0, -10, 0));
-
-
-		btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0, 1, 0), 1);
-
-		btCollisionShape* fallShape = new btSphereShape(1);
-
-
-		btDefaultMotionState* groundMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, -1, 0)));
-		btRigidBody::btRigidBodyConstructionInfo
-			groundRigidBodyCI(0, groundMotionState, groundShape, btVector3(0, 0, 0));
-		btRigidBody* groundRigidBody = new btRigidBody(groundRigidBodyCI);
-		dynamicsWorld->addRigidBody(groundRigidBody);
-
-
-		btDefaultMotionState* fallMotionState =
-			new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 50, 0)));
-		btScalar mass = 1;
-		btVector3 fallInertia(0, 0, 0);
-		fallShape->calculateLocalInertia(mass, fallInertia);
-		btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass, fallMotionState, fallShape, fallInertia);
-		btRigidBody* fallRigidBody = new btRigidBody(fallRigidBodyCI);
-		dynamicsWorld->addRigidBody(fallRigidBody);
-
-
-		for (int i = 0; i<300; i++) {
-			dynamicsWorld->stepSimulation(1 / 60.f, 10);
-
-			btTransform trans;
-			fallRigidBody->getMotionState()->getWorldTransform(trans);
-
-			std::cout << "sphere height: " << trans.getOrigin().getY() << std::endl;
-		}
-
-		dynamicsWorld->removeRigidBody(fallRigidBody);
-		delete fallRigidBody->getMotionState();
-		delete fallRigidBody;
-
-		dynamicsWorld->removeRigidBody(groundRigidBody);
-		delete groundRigidBody->getMotionState();
-		delete groundRigidBody;
-
-
-		delete fallShape;
-
-		delete groundShape;
-
-
-		delete dynamicsWorld;
-		delete solver;
-		delete collisionConfiguration;
-		delete dispatcher;
-		delete broadphase;
-
-	
-	
-	
-	
-	
-	
+	testBullet();
 	
 	int result = -1;
 
 
-  
-	
-
 	// setup application base settings
-    QApplication::setApplicationName("Troen");
+	QApplication::setApplicationName("Troen");
 	QApplication * application = new QApplication(argc, argv);
 
-    // create and open MainWindow (subclass of QMainWindow)
-    MainWindow * mainWindow = new MainWindow();
-    mainWindow->show();
+	// create and open MainWindow (subclass of QMainWindow)
+	MainWindow * mainWindow = new MainWindow();
+	mainWindow->show();
 
 	result = application->exec();
 
@@ -101,5 +94,7 @@ int main(int argc, char* argv[])
 	delete mainWindow;
 	delete application;
 
-  return result;
+	return result;
 }
+
+
