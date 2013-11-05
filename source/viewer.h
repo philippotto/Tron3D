a906bcbeb0fed25395f539c547054518aedb9e9b
MaxReimann <max.reimann@student.hpi.uni-potsdam.de>
2013-11-05 21:57:47 +0100

    fixed depth buffering issue, still only works in release mode

--- source/viewer.h
+++ source/viewer.h
@@ -3,19 +3,22 @@
 #include "forwarddeclarations.h"
 
 #include <QtGui/QtGui>
+#include <osgViewer/CompositeViewer>
 #include <QWidget>
+#include <osgQt/GraphicsWindowQt>
 
 #include <osg/ref_ptr>
 
 
-class Viewer : public QWidget
+class Viewer : public QWidget,  public osgViewer::CompositeViewer
 {
 	public:
 		Viewer();
 
 	protected:
 		virtual void paintEvent(QPaintEvent* event);
-		osg::Camera * createCamera(int x, int y, int w, int h);
+		osg::Camera * createCamera(osgQt::GraphicsWindowQt* gw, osgViewer::View* view);
+		osgQt::GraphicsWindowQt* createGraphicsWindow(int x, int y, int w, int h);
 
 		osg::ref_ptr<osgViewer::Viewer> m_viewer;
 		osg::ref_ptr<osg::Camera> m_camera;
