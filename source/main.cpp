87a3b43e6b17e12509fb6b89cca90cc32c269701
MaxReimann <max.reimann@student.hpi.uni-potsdam.de>
2014-02-24 17:03:48 +0100

    included raknet library into cmake

--- source/main.cpp
+++ source/main.cpp
@@ -8,8 +8,44 @@
 #define EXTERN
 #include "globals.h"
 
+
+#include <stdio.h>
+#include "RakPeerInterface.h"
+
+#define MAX_CLIENTS 10
+#define SERVER_PORT 60000
+
+
+
 int main(int argc, char* argv[])
 {
+
+	char str[512];
+	RakNet::RakPeerInterface *peer = RakNet::RakPeerInterface::GetInstance();
+	bool isServer;
+
+	printf("(C) or (S)erver?\n");
+	gets(str);
+	if ((str[0] == 'c') || (str[0] == 'C'))
+	{
+		RakNet::SocketDescriptor sd;
+		peer->Startup(1, &sd, 1);
+		isServer = false;
+	}
+	else {
+		RakNet::SocketDescriptor sd(SERVER_PORT, 0);
+		peer->Startup(MAX_CLIENTS, &sd, 1);
+		isServer = true;
+	}
+
+
+	// TODO - Add code body here
+
+	RakNet::RakPeerInterface::DestroyInstance(peer);
+
+	return 0;
+
+
 	int result = -1;
 
 	// register meta types
@@ -29,4 +65,7 @@ int main(int argc, char* argv[])
 	delete application;
 
 	return result;
+
+
+
 }
