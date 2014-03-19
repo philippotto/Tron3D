04074cad4930ea1442422d8eb67cf0bbff77ad11
Philipp Otto <philipp@scm.io>
2014-03-19 17:33:03 +0100

    #62 use multiple inheritance for AI

--- source/input/ai.cpp
+++ source/input/ai.cpp
@@ -14,12 +14,22 @@
 
 using namespace troen::input;
 
-
-
-AI::AI(osg::ref_ptr<BikeInputState> bikeInputState, troen::BikeController* bikeController) : PollingDevice(bikeInputState)
+AI::AI(osg::ref_ptr<BikeInputState> bikeInputState, troen::BikeController* bikeController)
+	: PollingDevice(bikeInputState),
+	  reflectionzeug::Object("player")
 {
 	m_bikeController = bikeController;
+	m_bikeInputState = bikeInputState;
 
+	addFunction("getFreeDistanceInDirection", this, &AI::getFreeDistanceInDirection);
+	addFunction("getID", this, &AI::getID);
+	addFunction("getHealth", this, &AI::getHealth);
+	addFunction("log", this, &AI::log);
+	addFunction("setTurbo", this, &AI::setTurbo);
+
+	addProperty<double>("acceleration", *this, &AI::getAcceleration, &AI::setAcceleration);
+	addProperty<double>("angle", *this, &AI::angle, &AI::setAngle);
+	addProperty<double>("distanceToNextObstacle", *this, &AI::distanceToNextObstacle, &AI::setDistanceToNextObstacle);
 }
 
 void AI::run()
@@ -28,23 +38,21 @@ void AI::run()
 
 	m_pollingEnabled = true;
 	
-	m_aiScript = new AIScript(m_bikeInputState, m_bikeController);
 	ScriptContext g_scriptingThread;
-	g_scriptingThread.registerObject(m_aiScript);
+	g_scriptingThread.registerObject(this);
 
 	ScriptWatcher scriptWatcher;
 	scriptWatcher.watchAndLoad("source/scripts/ai.js", &g_scriptingThread);
 	
 	while (m_pollingEnabled)
 	{
-		m_aiScript->setDistanceToNextObstacle(m_bikeController->getDistanceToObstacle(0));
+		setDistanceToNextObstacle(m_bikeController->getDistanceToObstacle(0));
 
 		reflectionzeug::Variant value;
 		value = g_scriptingThread.evaluate("try {     if (typeof move !== 'undefined') { move() } else { player.angle = 0 }      } catch (ex) { ex } ");
-		//std::cout << "result of script: " << value.toString() << std::endl;
-
-		m_bikeInputState->setAcceleration(m_aiScript->getAcceleration());
-		m_bikeInputState->setAngle(m_aiScript->angle());
+		
+		m_bikeInputState->setAcceleration(getAcceleration());
+		m_bikeInputState->setAngle(angle());
 
 		QCoreApplication::processEvents();
 		this->msleep(POLLING_DELAY_MS * 2);
--- source/input/ai.h
+++ source/input/ai.h
@@ -20,36 +20,24 @@ namespace troen
 {
 	namespace input
 	{
-		class AIScript : public reflectionzeug::Object
+		class AI : public reflectionzeug::Object, public PollingDevice
 		{
 		public:
-			AIScript(osg::ref_ptr<BikeInputState> bikeInputState, troen::BikeController* bikeController) : reflectionzeug::Object("player") {
+			AI(osg::ref_ptr<BikeInputState> bikeInputState, troen::BikeController* bikeController);
 
-				m_bikeController = bikeController;
-				m_bikeInputState = bikeInputState;
-
-				addFunction("getFreeDistanceInDirection", this, &AIScript::getFreeDistanceInDirection);
-				addFunction("getID", this, &AIScript::getID);
-				addFunction("getHealth", this, &AIScript::getHealth);
-				addFunction("log", this, &AIScript::log);
-				addFunction("setTurbo", this, &AIScript::setTurbo);
-				
-
-				addProperty<double>("acceleration", *this, &AIScript::getAcceleration, &AIScript::setAcceleration);
-				addProperty<double>("angle", *this, &AIScript::angle, &AIScript::setAngle);
-				addProperty<double>("distanceToNextObstacle", *this, &AIScript::distanceToNextObstacle, &AIScript::setDistanceToNextObstacle);
-			}
-
+			void run() override;
 
 			double getAcceleration() const { return m_acceleration; }
 			void setAcceleration(const double &acc) { m_acceleration = acc; }
-
 
 			double angle() const { return m_angle; }
 			void setAngle(const double & angle) { m_angle = angle; }
 
 			double distanceToNextObstacle() const { return m_distanceToNextObstacle; }
-			void setDistanceToNextObstacle(const double & distanceToNextObstacle) { m_distanceToNextObstacle = distanceToNextObstacle; }
+			
+			void setDistanceToNextObstacle(const double & distanceToNextObstacle) {
+				m_distanceToNextObstacle = distanceToNextObstacle;
+			}
 
 			double getFreeDistanceInDirection(double angle) {
 				double distance = m_bikeController->getDistanceToObstacle(angle);
@@ -60,17 +48,11 @@ namespace troen
 				std::cout << "script log:   " << message << std::endl;
 			}
 
-			void setTurbo(bool b) {
-				m_bikeInputState->setTurboPressed(b);
-			}
+			void setTurbo(bool b) { m_bikeInputState->setTurboPressed(b); }
 
-			int getID() {
-				return m_bikeController->player()->id();
-			}
+			int getID() { return m_bikeController->player()->id(); }
 
-			int getHealth() {
-				return m_bikeController->player()->health();
-			}
+			int getHealth() { return m_bikeController->player()->health(); }
 
 		protected:
 			double m_angle, m_acceleration, m_distanceToNextObstacle;
@@ -78,21 +60,6 @@ namespace troen
 			osg::ref_ptr<BikeInputState> m_bikeInputState;
 		};
 
-
-		class AI : public PollingDevice
-		{
-		public:
-			//AI(osg::ref_ptr<BikeInputState> bikeInputState);
-			AI(osg::ref_ptr<BikeInputState> bikeInputState, troen::BikeController* bikeController);
-
-			void run() override;
-
-
-		private:
-			AIScript *m_aiScript;
-			BikeController *m_bikeController;
-			
-		};
 
 	}
 }
